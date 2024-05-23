8b3fdb55fdcc3b2cd3f3546f271b3e12dbdc0015
yaseensdk <yassinesadik57@gmail.com>
2024-05-23 10:33:56 +0100

    build: Nav bar component
    Store creation/switching

---
+++ components/navbar.tsx
@@ -0,0 +1,33 @@
+import { UserButton } from '@clerk/nextjs'
+import { MainNav } from './main-nav'
+import StoreSwitcher from './store-switcher'
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+import prismadb from '@/lib/prismadb'
+
+const Navbar = async () => {
+  const { userId } = auth()
+
+  if (!userId) {
+    redirect('/sign-in')
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  })
+
+  return (
+    <div className="border-b">
+      <div className="flex items-center h-16 px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-4" />
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Navbar
