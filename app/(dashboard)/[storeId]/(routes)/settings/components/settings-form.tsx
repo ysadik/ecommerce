055ff1b5a8dd2e1cebdcbb067694897a8d7008f6
yaseensdk <yassinesadik57@gmail.com>
2024-05-23 12:41:02 +0100

    build: Settings page initial setup

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,96 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import Heading from '@/components/ui/heading'
+import { Input } from '@/components/ui/input'
+import { Separator } from '@/components/ui/separator'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Store } from '@prisma/client'
+import { Trash } from 'lucide-react'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+
+interface SettingsFormProps {
+  initialData: Store
+}
+
+const formSchema = z.object({
+  name: z.string().min(1),
+})
+
+type SettingsFormValues = z.infer<typeof formSchema>
+
+const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const [open, setOpen] = useState(false)
+
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  })
+
+  const onSubmit = async (data: SettingsFormValues) => {
+    console.log(data)
+  }
+
+  return (
+    <>
+      <div className="flex justify-between items-center">
+        <Heading title="Settings" description="Manage store preferences" />
+        <Button
+          disabled={form.formState.isSubmitting}
+          variant="destructive"
+          size="icon"
+          onClick={() => setOpen(true)}
+        >
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+
+      <Separator />
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={form.formState.isSubmitting}
+                      placeholder="Store name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button
+            disabled={form.formState.isSubmitting}
+            className="ml-auto"
+            type="submit"
+          >
+            Save changes
+          </Button>
+        </form>
+      </Form>
+    </>
+  )
+}
+export default SettingsForm
