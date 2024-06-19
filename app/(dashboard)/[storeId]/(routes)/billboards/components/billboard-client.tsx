23c9860463e909898bbf4987feea6579fbc12b41
yaseensdk <yassinesadik57@gmail.com>
2024-06-19 12:09:51 +0100

    build: ApiList component

--- app/(dashboard)/[storeId]/(routes)/billboards/components/billboard-client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/billboard-client.tsx
@@ -7,6 +7,7 @@ import { Plus } from 'lucide-react'
 import { useParams, useRouter } from 'next/navigation'
 import { BillboardColumn, columns } from './columns'
 import { DataTable } from '@/components/ui/data-table'
+import { ApiList } from '@/components/ui/api-list'
 
 interface BillboardClientProps {
   data: BillboardColumn[]
@@ -30,10 +31,11 @@ const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
           Add New
         </Button>
       </div>
-
       <Separator />
-
       <DataTable searchKey="label" columns={columns} data={data} />
+      <Heading title="API" description="API calls for Billboards" />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   )
 }
