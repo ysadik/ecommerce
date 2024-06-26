622d5dd6f84aa45af541f063a2d29eef362fd31f
yaseensdk <yassinesadik57@gmail.com>
2024-06-26 11:28:33 +0100

    build: Orders Entity

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/order-client.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Heading from '@/components/ui/heading'
+import { Separator } from '@/components/ui/separator'
+import { OrdersColumn, columns } from './columns'
+import { DataTable } from '@/components/ui/data-table'
+
+interface OrderClientProps {
+  data: OrdersColumn[]
+}
+
+const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
+  return (
+    <>
+      <Heading
+        title={`Orders (${data.length})`}
+        description="Manage orders for your store"
+      />
+      <Separator />
+      <DataTable searchKey="products" columns={columns} data={data} />
+    </>
+  )
+}
+export default OrderClient
