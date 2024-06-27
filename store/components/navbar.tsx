9d3301ede0216aea0da7b2ea3a59df1fb8aeab01
yaseensdk <yassinesadik57@gmail.com>
2024-06-27 18:21:01 +0100

    build: NavbarActions component

--- store/components/navbar.tsx
+++ store/components/navbar.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Container from './ui/container'
 import MainNav from './main-nav'
 import getCategories from '@/actions/get-categories'
+import NavbarActions from './navbar-actions'
 
 export const revalidate = 0
 
@@ -15,6 +16,7 @@ const Navbar = async () => {
             <p className="font-bold text-xl">STORE</p>
           </Link>
           <MainNav data={categories} />
+          <NavbarActions />
         </div>
       </Container>
     </div>
---
+++ store/components/ui/button.tsx
@@ -0,0 +1,29 @@
+import { cn } from '@/lib/utils'
+import { forwardRef } from 'react'
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, disabled, type = 'button', ...props }, ref) => {
+    return (
+      <button
+        className={cn(
+          `w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition`,
+          className
+        )}
+        ref={ref}
+      >
+        {children}
+      </button>
+    )
+  }
+)
+
+Button.displayName = 'Button'
+
+export default Button
+
+// forwardRef allows you to pass a ref from a parent component to a child component.
+// This is useful when you need to interact directly with the child's DOM node.
+// It simplifies accessing child component elements and enhances component reusability and abstraction.
