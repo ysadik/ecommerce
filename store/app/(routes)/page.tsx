875d9e58af8a9e78e0179b6a5044cae93c1535a5
yaseensdk <yassinesadik57@gmail.com>
2024-06-27 18:42:52 +0100

    build: Billboard component

--- store/app/(routes)/page.tsx
+++ store/app/(routes)/page.tsx
@@ -1,4 +1,17 @@
-const HomePage = () => {
-  return <div>HomePage</div>
+import getBillboard from '@/actions/get-billboard'
+import Billboard from '@/components/billboard'
+import Container from '@/components/ui/container'
+
+export const revalidate = 0
+
+const HomePage = async () => {
+  const billboard = await getBillboard('d8491cae-d3ca-428c-90b9-ce1e24923550')
+  return (
+    <Container>
+      <div className="space-x-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  )
 }
 export default HomePage
