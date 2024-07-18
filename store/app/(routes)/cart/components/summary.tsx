27200f6e6b9cf80aefa23b34a7f028c174517698
yaseensdk <yassinesadik57@gmail.com>
2024-07-18 18:25:26 +0100

    func: stripe & checkout

--- store/app/(routes)/cart/components/summary.tsx
+++ store/app/(routes)/cart/components/summary.tsx
@@ -3,16 +3,41 @@
 import Button from '@/components/ui/button'
 import Currency from '@/components/ui/currency'
 import useCart from '@/hooks/use-cart'
+import axios from 'axios'
 import { useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+import toast from 'react-hot-toast'
 
 const Summary = () => {
   const searchParams = useSearchParams()
   const items = useCart((state) => state.items)
   const removeAll = useCart((state) => state.removeAll)
 
+  useEffect(() => {
+    if (searchParams.get('success')) {
+      toast.success('Payment completed.')
+      removeAll()
+    }
+
+    if (searchParams.get('canceled')) {
+      toast.error('Something went wrong.')
+    }
+  }, [searchParams, removeAll])
+
   const totalPrice = items.reduce((total, item) => {
     return total + Number(item.price)
   }, 0)
+
+  const onCheckout = async () => {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
+      {
+        productIds: items.map((item) => item.id),
+      }
+    )
+
+    window.location = response.data.url
+  }
 
   return (
     <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
@@ -23,7 +48,9 @@ const Summary = () => {
           <Currency value={totalPrice} />
         </div>
       </div>
-      <Button className="w-full mt-6">Checkout</Button>
+      <Button onClick={onCheckout} className="w-full mt-6">
+        Checkout
+      </Button>
     </div>
   )
 }
