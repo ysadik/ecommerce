47c27a4a5d1702d445c0456ce88717e61abbf592
yaseensdk <yassinesadik57@gmail.com>
2024-07-19 10:42:28 +0100

    func: Dark mode

--- admin/components/navbar.tsx
+++ admin/components/navbar.tsx
@@ -4,6 +4,7 @@ import StoreSwitcher from './store-switcher'
 import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
 import prismadb from '@/lib/prismadb'
+import { ThemeToggle } from './theme-toggle'
 
 const Navbar = async () => {
   const { userId } = auth()
@@ -24,6 +25,7 @@ const Navbar = async () => {
         <StoreSwitcher items={stores} />
         <MainNav className="mx-4" />
         <div className="ml-auto flex items-center space-x-4">
+          <ThemeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
